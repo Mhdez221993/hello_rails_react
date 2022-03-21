a2b467f34bc2948ba7d9dfbf795b0256493c77f3
Moises <mhdezcoronado@gmail.com>
2022-03-21 13:07:40 +0100

    Create the App component with react-router and render it in your static view

---
+++ app/javascript/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+class App extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Hi form App component</h1>
+      </React.Fragment>
+    );
+  }
+}
+
+export default App

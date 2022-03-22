f8b75f0447c5129e58881a1d358751010f16a3ef
Moises <mhdezcoronado@gmail.com>
2022-03-22 11:53:27 +0100

    Remove unnessessary files

--- app/controllers/api/messages_controller.rb
+++
@@ -1,6 +0,0 @@
-class Api::MessagesController < ApplicationController
-  def index
-    message = Message.order(Arel.sql('RANDOM()')).first
-    render json: { message: message.greeting }
-  end
-end
--- app/javascript/redux/greetings/api.js
+++
@@ -1,5 +0,0 @@
-export const getRandomGreeting = async () => {
-  const response = await fetch('api/greeting');
-
-  return response.json();
-}
